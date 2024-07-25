ee406f205d676f250bd6e69d9a8d7c1c84a5db38
kivid <1695949332@qq.com>
2024-07-25 12:25:02 +0800

    Add files via upload

---
+++ yuapi-frontend-master/README.md
@@ -0,0 +1,5 @@
+# 鱼皮 - API 开放平台
+
+项目地址：https://t.zsxq.com/09RWRlUU7
+
+加入编程导航知识星球可看完整教程，鱼皮为你答疑：https://yupi.icu
---
+++ yuapi-frontend-master/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+const Footer: React.FC = () => {
+  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: '鱼皮接口',
+          title: '鱼皮接口',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: '鱼皮接口',
+          title: '鱼皮接口',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
